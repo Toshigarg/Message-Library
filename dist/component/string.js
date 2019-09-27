f6a38555c1820df296128ef9a559426624550ee5
Saurabh Srivastava <ssrivastava@attune.gshapps.net>
2019-09-27 18:15:24 +0530

    first commit

--- dist/component/string.js
+++ dist/component/string.js
@@ -1,4 +1,11 @@
-export default function Message(name) {
+exports.global = function () {
+    return {
+        add: function (arg1, arg2) {
 
-    return name;
-}
+            return arg1 + arg2;
+        },
+        subtract: function (arg3, arg4) {
+            return arg3 - arg4;
+        }
+    };
+};
--- src/lib/component/string.js
+++ src/lib/component/string.js
@@ -1,4 +1,11 @@
-export default function Message(name) {
-
-    return name;
-}
+exports.global = function(){
+    return {
+         add : function(arg1, arg2){
+             
+			 return arg1 + arg2;
+         },
+         subtract : function (arg3, arg4){
+             return arg3 - arg4;
+         }
+   };
+ }
